c4063b54469075690fb204889ffcb0e0846e9ae3
tomaskls <tomaskls@hotmail.com>
2024-10-04 10:57:34 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Design } from './components/Design'
 import { Header } from './components/Header'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
     <Header />
+    <Design />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,43 @@
-import  './Header.css'
+import { useEffect, useState } from 'react';
+import './Header.css';
 
-export function Header () {
-    return (
-        <div className="header">
-    <div className="headerUp">
-      <img className="logo" src="/logo.jpg"/>
+export function Header() {
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const headerUp = document.querySelector('.headerUp');
+      if (headerUp) {
+        const headerUpHeight = headerUp.getBoundingClientRect().height;
+        setIsSticky(window.scrollY > headerUpHeight);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div className="header">
+      <div className="headerUp">
+        <img className="logo" src="/logo.jpg" alt="Logo"/>
+      </div>
+      <div className={`stickyHeader ${isSticky ? 'sticky' : ''}`}>
+        <div className="headerD">
+          <div className="leftSide">
+            <img className="logo" src="/blogo.png" alt="Brand Logo"/>
+          </div>
+          <div className="rightSide">
+            <a href="#vision">Vision</a>
+            <a href="#design">Design</a>
+            <a href="#inovation">Inovation</a>
+            <button className="buyButton">Buy</button>
+          </div>
+        </div>
+      </div>
     </div>
-    <div className="stickyHeader">
-    <div className="headerD">
-    <div className="leftSide"><img className="logo" src="/blogo.png"/></div>
-    <div className="rightSide">
-      <p className="navOption">Vision</p>
-      <p className="navOption">Design</p>
-      <p className="navOption">Innovation</p>
-      <button className="buyButton">Buy</button>
-    </div>
-    </div>
-  </div>
-  </div>
-    )
+  );
 }
