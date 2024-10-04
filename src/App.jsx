7919a8a00a00e8661427cb5e90be10ded8bcfdf0
Evaldas Petkus <evaldas.db@gmail.com>
2024-10-04 11:20:00 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Cool from './components/Cool.jsx';
-import Just from './components/Just.jsx';
-import Footer from './components/Footer.jsx';
+
 import './App.css';
 
 const App = () => {
